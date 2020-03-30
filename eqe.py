39820a0f19a511e22e20dde1c874155f5f47a189
jmball <jmball@users.noreply.github.com>
2020-03-30 19:07:36 +0100

    incorporated changes to lock-in init, connect, and config in lib

--- eqe.py
+++ eqe.py
@@ -105,217 +105,6 @@ def set_wavelength(wl, grating_change_wls=None, filter_change_wls=None):
     log_monochromator_response(resp)
 
 
-def configure_lockin(
-    input_configuration=0,
-    input_coupling=0,
-    ground_shielding=1,
-    line_notch_filter_status=3,
-    ref_source=0,
-    detection_harmonic=1,
-    ref_trigger=1,
-    ref_freq=1000,
-    sensitivity=26,
-    reserve_mode=1,
-    time_constant=8,
-    low_pass_filter_slope=1,
-    sync_status=0,
-    ch1_display=1,
-    ch2_display=1,
-    ch1_ratio=0,
-    ch2_ratio=0,
-):
-    """Configure lock-in amplifier settings.
-
-    Parameters
-    ----------
-    input_configuration : {0, 1, 2, 3}
-        Input configuration:
-
-            * 0 : A
-            * 1 : A-B
-            * 2 : I (1 MOhm)
-            * 3 : I (100 MOhm)
-    input_coupling : {0, 1}
-        Input coupling:
-
-            * 0 : AC
-            * 1 : DC
-    ground_shielding : {0, 1}
-        Input shield grounding:
-
-            * 0 : Float
-            * 1 : Ground
-    line_notch_filter_status : {0, 1, 2, 3}
-        Input line notch filter status:
-
-            * 0 : no filters
-            * 1 : Line notch in
-            * 2 : 2 x Line notch in
-            * 3 : Both notch filters in
-    ref_source : {0, 1}
-        Refernce source:
-
-            * 0 : external
-            * 1 : internal
-    detection_harmonic : int
-        Detection harmonic, 1 =< harmonic =< 19999.
-    ref_trigger : {0, 1, 2}
-        Trigger type:
-
-            * 0: zero crossing
-            * 1: TTL rising egde
-            * 2: TTL falling edge
-    ref_freq : float
-        Frequency in Hz, 0.001 =< freq =< 102000.
-    sensitivity : {0 - 26}
-        Sensitivity in V/uA:
-
-            * 0 : 2e-9
-            * 1 : 5e-9
-            * 2 : 10e-9
-            * 3 : 20e-9
-            * 4 : 50e-9
-            * 5 : 100e-9
-            * 6 : 200e-9
-            * 7 : 500e-9
-            * 8 : 1e-6
-            * 9 : 2e-6
-            * 10 : 5e-6
-            * 11 : 10e-6
-            * 12 : 20e-6
-            * 13 : 50e-6
-            * 14 : 100e-6
-            * 15 : 200e-6
-            * 16 : 500e-6
-            * 17 : 1e-3
-            * 18 : 2e-3
-            * 19 : 5e-3
-            * 20 : 10e-3
-            * 21 : 20e-3
-            * 22 : 50e-3
-            * 23 : 100e-3
-            * 24 : 200e-3
-            * 25 : 500e-3
-            * 26 : 1
-    reserve_mode : {0, 1, 2}
-        Reserve mode:
-
-            * 0 : High reserve
-            * 1 : Normal
-            * 2 : Low noise
-    time_constant : {0 - 19}
-        Time constant in s:
-
-            * 0 : 10e-6
-            * 1 : 30e-6
-            * 2 : 100e-6
-            * 3 : 300e-6
-            * 4 : 1e-3
-            * 5 : 3e-3
-            * 6 : 10e-3
-            * 7 : 30e-3
-            * 8 : 100e-3
-            * 9 : 300e-3
-            * 10 : 1
-            * 11 : 3
-            * 12 : 10
-            * 13 : 30
-            * 14 : 100
-            * 15 : 300
-            * 16 : 1e3
-            * 17 : 3e3
-            * 18 : 10e3
-            * 19 : 30e3
-    low_pass_filter_slope : {0, 1, 2, 3}
-        Low pass filter slope in dB/oct:
-
-            * 0 : 6
-            * 1 : 12
-            * 2 : 18
-            * 3 : 24
-    sync_status : {0, 1}
-        Synchronous filter status:
-
-            * 0 : Off
-            * 1 : below 200 Hz
-    ch1_display : {0, 1, 2, 3, 4}
-        Display parameter for CH1:
-
-            * 0 : X
-            * 1 : R
-            * 2 : X Noise
-            * 3 : Aux In 1
-            * 4 : Aux In 2
-    ch2_display : {0, 1, 2, 3, 4}
-        Display parameter for CH2:
-
-            * 0 : Y
-            * 1 : Phase
-            * 2 : Y Noise
-            * 3 : Aux In 3
-            * 4 : Aux In 4
-    ch1_ratio : {0, 1, 2}
-        Ratio type for CH1:
-
-            * 0 : none
-            * 1 : Aux In 1
-            * 2 : Aux In 2
-    ch2_ratio : {0, 1, 2}
-        Ratio type for CH1:
-
-            * 0 : none
-            * 1 : Aux In 3
-            * 2 : Aux In 4
-
-    Returns
-    -------
-    """
-    lockin.set_input_configuration(input_configuration)
-    log_lockin_response(f"set_input_configuration({input_configuration})")
-
-    lockin.set_input_coupling(input_coupling)
-    log_lockin_response(f"set_input_coupling({input_coupling})")
-
-    lockin.set_input_shield_gnd(ground_shielding)
-    log_lockin_response(f"set_input_shield_gnd({ground_shielding})")
-
-    lockin.set_line_notch_status(line_notch_filter_status)
-    log_lockin_response(f"set_line_notch_status({line_notch_filter_status})")
-
-    lockin.set_ref_source(ref_source)
-    log_lockin_response(f"set_ref_source({ref_source})")
-
-    lockin.set_harmonic(detection_harmonic)
-    log_lockin_response(f"set_detection_harmonic({detection_harmonic})")
-
-    lockin.set_reference_trigger(ref_trigger)
-    log_lockin_response(f"set_reference_trigger({ref_trigger})")
-
-    lockin.set_ref_freq(ref_freq)
-    log_lockin_response(f"set_ref_freq({ref_freq})")
-
-    lockin.set_sensitivity(sensitivity)
-    log_lockin_response(f"set_sensitivity({sensitivity})")
-
-    lockin.set_reserve_mode(reserve_mode)
-    log_lockin_response(f"set_reserve({reserve_mode})")
-
-    lockin.set_time_constant(time_constant)
-    log_lockin_response(f"set_time_constant({time_constant})")
-
-    lockin.set_lp_filter_slope(low_pass_filter_slope)
-    log_lockin_response(f"set_lp_filter_slope({low_pass_filter_slope})")
-
-    lockin.set_sync_status(sync_status)
-    log_lockin_response(f"set_sync_status({sync_status})")
-
-    lockin.set_display(1, ch1_display, ch1_ratio)
-    log_lockin_response(f"set_display(1, {ch1_display}, {ch1_ratio})")
-
-    lockin.set_display(2, ch2_display, ch2_ratio)
-    log_lockin_response(f"set_display(2, {ch2_display}, {ch2_ratio})")
-
-
 def measure(
     wl,
     grating_change_wls=None,
@@ -637,19 +426,13 @@ logger.debug(f"Monochromator filter change wls: {mono_filter_change_wls}")
 logger.debug(f"Monochromator scan speed: {mono_scan_speed}")
 
 # instantiate instrument objects and connect
-lockin = sr830.sr830(address=lia_address, output_interface=lia_output_interface)
-lockin.connect()
-lockin_sn = lockin.serial_number
-sensitivities = lockin.sensitivities
-time_constants = lockin.time_constants
-mono = sp2150.sp2150(address=mono_address)
-mono.connect()
-logger.info(
-    f"{lockin.manufacturer}, {lockin.model}, {lockin_sn}, {lockin.firmware_version} connected!"
+lockin = sr830.sr830(return_int=True, check_errors=True)
+lockin.connect(
+    resource_name=lia_address,
+    output_interface=lia_output_interface,
+    set_default_configuration=False,
 )
-
-# configure instruments
-configure_lockin(
+lockin.set_configuration(
     input_configuration=lia_input_configuration,
     input_coupling=lia_input_coupling,
     ground_shielding=lia_ground_shielding,
@@ -668,15 +451,17 @@ configure_lockin(
     ch1_ratio=lia_ch1_ratio,
     ch2_ratio=lia_ch2_ratio,
 )
-configure_monochromator(scan_speed=mono_scan_speed)
+lockin_sn = lockin.serial_number
+sensitivities = lockin.sensitivities
+time_constants = lockin.time_constants
+mono = sp2150.sp2150(address=mono_address)
+mono.connect()
+logger.info(
+    f"{lockin.manufacturer}, {lockin.model}, {lockin_sn}, {lockin.firmware_version} connected!"
+)
 
-# data = measure(
-#     600,
-#     grating_change_wls=mono_grating_change_wls,
-#     filter_change_wls=mono_filter_change_wls,
-#     auto_gain=lia_auto_gain,
-#     auto_gain_method=lia_auto_gain_method,
-# )
+# configure instruments
+configure_monochromator(scan_speed=mono_scan_speed)
 
 # run scan
 scan(
