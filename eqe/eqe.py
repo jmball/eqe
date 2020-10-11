d5e617bb686e7604e35b2f69fdc4e3874bb62a27
Greyson Christoforo <grey@christoforo.net>
2020-10-11 11:52:04 +0100

    change default current limit

--- eqe/eqe.py
+++ eqe/eqe.py
@@ -181,7 +181,7 @@ def scan(
     psu_ch3_voltage=0,
     psu_ch3_current=0,
     smu_voltage=0,
-    smu_compliance=1,
+    smu_compliance=0.1,
     start_wl=350,
     end_wl=1100,
     num_points=76,
